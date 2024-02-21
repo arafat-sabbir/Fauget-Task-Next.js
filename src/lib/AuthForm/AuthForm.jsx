380cd5513a1d8375751384eb41e7beac964b0f34
arafat-sabbir <arafatshabbir8@gmail.com>
2024-02-22 03:42:43 +0600

    Added Sign in and SignUp form Submit Hanlder

--- src/lib/AuthForm/AuthForm.jsx
+++ src/lib/AuthForm/AuthForm.jsx
@@ -1,11 +1,23 @@
 import { DialogContent, DialogHeader, DialogTitle, DialogDescription } from "@/components/ui/dialog";
 
 const AuthForm = ({ formType, setFormType }) => {
-    const handleSignIn = ()=>{
+    const handleSignIn = (e) => {
+        e.preventDefault()
+        const form = e.target
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+        form.reset()
 
     }
-    const handleSignUp = ()=>{
-        
+    const handleSignUp = (e) => {
+        e.preventDefault()
+        const form = e.target
+        const email = form.email.value;
+        const password = form.password.value;
+        const confirmPassword = form.confirmPassword.value;
+        console.log(email, password, confirmPassword);
+        form.reset()
     }
     return (
         <div>
@@ -16,22 +28,22 @@ const AuthForm = ({ formType, setFormType }) => {
                     <DialogDescription>
                         {formType === 'signIn' ?
                             <form onSubmit={handleSignIn} className="flex flex-col px-10 bg-[#3B3B3B]">
-                                <label className="flex" htmlFor="email">Name: </label>
+                                <label className="text-white ml-[5px]" htmlFor="email">Email: </label>
                                 <input className="input-field" type="email" name="email" />
-                                <label htmlFor="password">Password:</label>
+                                <label className="text-white ml-[5px]" htmlFor="password">Password:</label>
                                 <input className="input-field" type="password" name="password" />
                                 <input className="btn my-6 w-1/2 text-center bg-[#060606] cursor-pointer " type="submit" value="Sign In" />
-                                <p className="text-gray-400">Do not Have A Account? Create an Account  <span className="cursor-pointer text-[#060606] underline" onClick={() => setFormType("signUp")}>SignUp</span></p>
+                                <p className="text-gray-200 ml-1">Do not Have A Account? Create an Account  <span className="cursor-pointer text-black underline font-semibold text-lg" onClick={() => setFormType("signUp")}>SignUp</span></p>
                             </form>
                             : (<form onSubmit={handleSignUp} className="flex flex-col px-10 bg-[#3B3B3B]">
-                                <label className="flex" htmlFor="email">Name: </label>
+                                <label className="text-white ml-[5px]" htmlFor="email text-gray-200">Email: </label>
                                 <input className="input-field" type="email" name="email" />
-                                <label htmlFor="password">Password:</label>
+                                <label className="text-white ml-[5px]" htmlFor="password">Password:</label>
                                 <input className="input-field" type="password" name="password" />
-                                <label htmlFor="password">Confirm Password:</label>
-                                <input className="input-field" type="password" name="password" />
+                                <label className="text-white ml-[5px]" htmlFor="password">Confirm Password:</label>
+                                <input className="input-field" type="password" name="confirmPassword" />
                                 <input className="btn my-6 w-1/2 text-center bg-[#060606] cursor-pointer " type="submit" value="Sign Up" />
-                                <p className="text-gray-400">Have account? Sign in  account   <span className="cursor-pointer text-[#060606] underline" onClick={() => setFormType("signIn")}>signIn</span></p>
+                                <p className="text-gray-200 ml-1">Have account? Sign in  account   <span className="cursor-pointer text-black underline font-semibold text-lg" onClick={() => setFormType("signIn")}>SignIn</span></p>
                             </form>)}
                     </DialogDescription>
                 </DialogHeader>
