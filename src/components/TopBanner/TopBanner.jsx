810b8714f0cb0b25e4ae3a24ea3ba93a69efa572
unknown <arafatshabbir8@gmail.com>
2024-02-24 13:53:16 +0600

    Added Conditional Banner On the home section

--- src/components/TopBanner/TopBanner.jsx
+++ src/components/TopBanner/TopBanner.jsx
@@ -1,16 +1,41 @@
 import { useSession } from "next-auth/react";
+import { Niconne } from "next/font/google";
 import Image from 'next/image';
+import { PiCrownSimpleLight } from "react-icons/pi";
+
+// Call the font loader and assign it to a constant at the module level
+const niconneFont = Niconne({ weight: "400", subsets: ["latin"], display: "swap" });
+
 const TopBanner = () => {
     const { data: session } = useSession()
     return (
         <div>
             {
                 session ?
-                    <><div className="bg-[url('https://i.ibb.co/4Z76xzx/Rectangle-2.png')] bg-cover bg-no-repeat h-[332px] w-full mt-16">
-                        jkladfk
-                    </div></>
+                    <div className="bg-[url('https://i.ibb.co/4Z76xzx/Rectangle-2.png')] relative px-6 pt-4 bg-cover bg-no-repeat h-[332px] w-full mt-16">
+                        <p className="flex items-center gap-4 text-white text-2xl font-semibold tracking-wide"> <PiCrownSimpleLight className="text-white" size={56} /> No Ads & Unlock All Paid Songs </p>
+                        {/* Use the className property from the font loader */}
+                        <p className={`${niconneFont.className} text-5xl font-semibold text-white`}>Premium Membership</p>
+                        <p className="text-lg mt-4 w-[775px] text-white font-semibold tracking-wide">It is a long established fact that a is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English.</p>
+                        <div className="flex gap-4 mt-10">
+                            <button className="btn bg-[#060606]">Upgrade Now</button>
+                            <button className="btn bg-[#044C57]">Learn more</button>
+                        </div>
+                        <div className="bg-[url('https://i.ibb.co/GHvy82F/image-2.png')] h-72 bg-top w-44 absolute right-4 bottom-0 bg-cover">
+
+                        </div>
+                    </div>
                     :
-                    ""
+                    <div className="bg-[url('https://i.ibb.co/jJC4L5K/apixcel-home-banner.webp')] relative px-6 pt-4 bg-cover bg-no-repeat h-[332px] w-full mt-16">
+                        <p className="flex items-center gap-4 text-white text-2xl font-semibold tracking-wide"> <PiCrownSimpleLight className="text-white" size={56} /> No Ads & Unlock All Paid Songs </p>
+                        {/* Use the className property from the font loader */}
+                        <p className={`${niconneFont.className} text-5xl font-semibold text-white`}>Premium Membership</p>
+                        <p className="text-lg mt-4 w-[775px] text-white font-semibold tracking-wide">It is a long established fact that a is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English.</p>
+                        <div className="flex gap-4 mt-10">
+                            <button className="btn bg-[#060606]">Upgrade Now</button>
+                            <button className="btn bg-[#044C57]">Learn more</button>
+                        </div>
+                    </div>
             }
         </div>
     );
